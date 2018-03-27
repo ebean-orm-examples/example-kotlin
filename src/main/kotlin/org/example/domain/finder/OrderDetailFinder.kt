0bc9f87083b13ea174fa4d588b5755f4535f3f81
Rob Bygrave <robin.bygrave@gmail.com>
2018-03-27 16:32:21 +1300

    bump ebean to 11.15.1 and kotlin 1.2.30

--- src/main/kotlin/org/example/domain/finder/OrderDetailFinder.kt
+++ src/main/kotlin/org/example/domain/finder/OrderDetailFinder.kt
@@ -4,31 +4,14 @@ import io.ebean.Finder
 import org.example.domain.OrderDetail
 import org.example.domain.query.QOrderDetail
 
-open class OrderDetailFinder : Finder<Long,OrderDetail> {
+open class OrderDetailFinder : Finder<Long, OrderDetail>(OrderDetail::class.java) {
 
   val alias = QOrderDetail._alias
-
-  /**
-   * Construct using the default EbeanServer.
-   */
-  constructor() : super(OrderDetail::class.java)
-
-  /**
-   * Construct with a given EbeanServer.
-   */
-  constructor(serverName: String) : super(OrderDetail::class.java, serverName)
 
   /**
    * Start a new typed query.
    */
   fun where(): QOrderDetail {
      return QOrderDetail(db())
-  }
-
-  /**
-   * Start a new document store query.
-   */
-  fun text(): QOrderDetail {
-     return QOrderDetail(db()).text()
   }
 }
